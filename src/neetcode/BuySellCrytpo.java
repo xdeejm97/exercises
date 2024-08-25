163aee87f138ed596d7cdcb2aa0261197f4bb292
xdeejm97 <d.niesmialowski@gmail.com>
2024-08-25 19:58:16 +0200

    Crytpo buy and sale

---
+++ src/neetcode/BuySellCrytpo.java
@@ -0,0 +1,25 @@
+package neetcode;
+
+public class BuySellCrytpo {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{10,1,5,6,7,1}));
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        int l = 0;
+        int r = 1;
+        int maxProfit = 0;
+        while (r < prices.length){
+
+            if(prices[l] < prices[r]){
+                maxProfit = Math.max(maxProfit, prices[r] - prices[l]);
+            }else {
+                l = r;
+            }
+            r++;
+        }
+        return maxProfit;
+
+    }
+}
