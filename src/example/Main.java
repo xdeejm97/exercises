8c3ec823c7d729092a3163231d88164048ca341e
xdeejm97 <d.niesmialowski@gmail.com>
2024-09-23 22:36:41 +0200

    Examples

--- src/example/Main.java
+++ src/example/Main.java
@@ -5,8 +5,17 @@ public class Main {
 
         Car car = new Car("volvo");
         Car car2 = new Car("volvo");
+        long a = 5;
+        long a1 = 15;
+        byte b = (byte) ((byte) a/a1);
 
         System.out.println(car == car2);
+        int v = 1/3;
+        System.out.println("2+2 = " +2+2);
+        System.out.println(v);
+        double p = 1.3d;
+        System.out.println(p);
+        float m = 1/3;
 
     }
 }
